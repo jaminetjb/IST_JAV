cb98c28e277afa4f2dacbc2a483889dd0c344321
Jacob Jaminet <jacob.jaminet@gmail.com>
2017-10-02 12:10:58 -0400

    Add files via upload

---
+++ Project2/ElectricCar.java
@@ -0,0 +1,13 @@
+
+public class ElectricCar extends Car {
+
+	public ElectricCar() {
+		super();
+		isElectric = true;
+	}
+	
+	public ElectricCar(String license, int yr, int decal, String make, String model) {
+		super();
+		isElectric = true;
+	}
+}
---
+++ Project2/HandicapCar.java
@@ -0,0 +1,14 @@
+
+public class HandicapCar extends Car {
+
+	public HandicapCar() {
+		super();
+		isHandicap = true;
+	}
+	
+	public HandicapCar(String license, int yr, int decal, String make, String model) {
+		super();
+		isHandicap = true;
+	}
+
+}
